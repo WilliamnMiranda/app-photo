0706bc1ccdb48f977ec9b26e44617d205257a050
William Nascimento <williamnnmiranda@gmail.com>
2023-09-04 13:08:56 -0300

    refactor: :recycle: refactored pin component

--- src/components/pin_code/index.tsx
+++ src/components/pin_code/index.tsx
@@ -2,31 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import * as C from "./styles";
 import useCodePin from "../../hooks/useCodePin";
 const PinCode = () => {
-  const PIN_COUNT = 10;
-  const [inputValues, setInputValues] = useState(Array(PIN_COUNT).fill(""));
-  const inputRefs = useRef(Array(PIN_COUNT).fill(null));
-  const { enterToStorage } = useCodePin();
-  useEffect(() => {
-    inputRefs.current[0]?.focus();
-  }, []);
-
-  const handleInput = (
-    index: number,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const value = event.target.value;
-    const newInputValues = [...inputValues];
-    newInputValues[index] = value;
-
-    setInputValues(newInputValues);
-
-    if (value && index < PIN_COUNT - 1) {
-      inputRefs.current[index + 1]?.focus();
-    } else if (!value && index > 0) {
-      inputRefs.current[index - 1]?.focus();
-    }
-  };
-
+  const { enterToStorage,inputValues,setInputValues,PIN_COUNT,handleInput,inputRefs} = useCodePin();
   const submit = () => {
     const pinString = inputValues.join(""); // Transforma o array em uma string
     enterToStorage(pinString);
